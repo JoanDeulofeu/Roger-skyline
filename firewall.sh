65b78d6e036fabbda9bfa4070d5a901d1576012c
JoanDeulofeu <jgehin@student.42.fr>
2019-06-11 15:04:22 +0200

    sudo

--- firewall.sh
+++ firewall.sh
@@ -9,50 +9,50 @@ SERVICE=service
 IFACE_LAN=enp0s8
 
 # Tout accepter
-$IPT -t filter -P INPUT ACCEPT
-$IPT -t filter -P FORWARD ACCEPT
-$IPT -t filter -P OUTPUT ACCEPT
-$IPT -t nat -P PREROUTING ACCEPT
-$IPT -t nat -P POSTROUTING ACCEPT
-$IPT -t nat -P OUTPUT ACCEPT
-$IPT -t mangle -P PREROUTING ACCEPT
-$IPT -t mangle -P INPUT ACCEPT
-$IPT -t mangle -P FORWARD ACCEPT
-$IPT -t mangle -P OUTPUT ACCEPT
-$IPT -t mangle -P POSTROUTING ACCEPT
+sudo $IPT -t filter -P INPUT ACCEPT
+sudo $IPT -t filter -P FORWARD ACCEPT
+sudo $IPT -t filter -P OUTPUT ACCEPT
+sudo $IPT -t nat -P PREROUTING ACCEPT
+sudo $IPT -t nat -P POSTROUTING ACCEPT
+sudo $IPT -t nat -P OUTPUT ACCEPT
+sudo $IPT -t mangle -P PREROUTING ACCEPT
+sudo $IPT -t mangle -P INPUT ACCEPT
+sudo $IPT -t mangle -P FORWARD ACCEPT
+sudo $IPT -t mangle -P OUTPUT ACCEPT
+sudo $IPT -t mangle -P POSTROUTING ACCEPT
 
 # Remettre les compteurs à zéro
-$IPT -t filter -Z
-$IPT -t nat -Z
-$IPT -t mangle -Z
+sudo $IPT -t filter -Z
+sudo $IPT -t nat -Z
+sudo $IPT -t mangle -Z
 
 # Supprimer toutes les règles actives et les chaînes personnalisées
-$IPT -t filter -F
-$IPT -t filter -X
-$IPT -t nat -F
-$IPT -t nat -X
-$IPT -t mangle -F
-$IPT -t mangle -X
+sudo $IPT -t filter -F
+sudo $IPT -t filter -X
+sudo $IPT -t nat -F
+sudo $IPT -t nat -X
+sudo $IPT -t mangle -F
+sudo $IPT -t mangle -X
 
 # Politique par défaut
-$IPT -P INPUT DROP
-$IPT -P FORWARD DROP
-$IPT -P OUTPUT ACCEPT
+sudo $IPT -P INPUT DROP
+sudo $IPT -P FORWARD DROP
+sudo $IPT -P OUTPUT ACCEPT
 
 # Faire confiance à nous-mêmes ;o)
-$IPT -A INPUT -i lo -j ACCEPT
+sudo $IPT -A INPUT -i lo -j ACCEPT
 
 
 # Ping
-$IPT -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
-$IPT -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
-$IPT -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
+sudo $IPT -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+sudo $IPT -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
+sudo $IPT -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
 
 # Connexions établies
-$IPT -A INPUT -m state --state ESTABLISHED -j ACCEPT
+sudo $IPT -A INPUT -m state --state ESTABLISHED -j ACCEPT
 
 # SSH
-$IPT -A INPUT -p tcp -i $IFACE_LAN --dport 42 -j ACCEPT
+sudo $IPT -A INPUT -p tcp -i $IFACE_LAN --dport 42 -j ACCEPT
 
 # Enregistrer la configuration
-$SERVICE iptables save
+sudo $SERVICE iptables save
