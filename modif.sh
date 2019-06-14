fd43b216b837d9ad09830f1de4065f55f01382d6
JoanDeulofeu <jgehin@student.42.fr>
2019-06-14 15:05:19 +0200

    tg romain

--- modif.sh
+++ modif.sh
@@ -6,12 +6,11 @@
 PATH_FILE="/etc/"
 NAME_FILE="crontab"
 NOM_FICHIER="/etc/crontab"
-MAIL_ADMIN="joan@outlook.fr"
+MAIL_ADMIN="root"
 BOOL_MODIF=$(find $PATH_FILE -name $NAME_FILE -mtime -1 -print)
 
 
 if [ "${BOOL_MODIF}" != NULL ]; then
 	CORPS_MESSAGE="Alerte, le fichier ${NOM_FICHIER} a été modifié";
-	echo "$CORPS_MESSAGE" | mail -s "Alerte surveillance modification de fichier" "${MAIL_ADMIN}";
-	echo "mdr lol\n"
+	echo "$CORPS_MESSAGE" | mail -s "Alerte surveillance modification de fichier" ${MAIL_ADMIN};
 fi
