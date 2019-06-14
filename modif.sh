0792f7c05a09999a8f7a798071343e2bb08e2eb0
JoanDeulofeu <jgehin@student.42.fr>
2019-06-14 15:22:03 +0200

    lol

--- modif.sh
+++ modif.sh
@@ -13,4 +13,5 @@ BOOL_MODIF=$(find $PATH_FILE -name $NAME_FILE -mtime -1 -print)
 if [ "${BOOL_MODIF}" != NULL ]; then
 	CORPS_MESSAGE="Alerte, le fichier ${NOM_FICHIER} a été modifié";
 	echo "$CORPS_MESSAGE" | mail -s "Alerte surveillance modification de fichier" ${MAIL_ADMIN};
+	echo "lol\n"
 fi
