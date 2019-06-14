8c2a4428d9cbed5111cc18a7be3ef32f2f5f711a
JoanDeulofeu <jgehin@student.42.fr>
2019-06-14 15:00:38 +0200

    log.sh -y

--- log.sh
+++ log.sh
@@ -6,6 +6,6 @@ DATE=$(date +"%m-%d-%y")
 HOUR=$(date +"%r")
 
 echo $DATE $HOUR '\n' >> /var/log/update_script.log
-sudo apt-get update >> /var/log/update_script.log
-sudo apt-get upgrade >> /var/log/update_script.log
+sudo apt-get update -y >> /var/log/update_script.log
+sudo apt-get upgrade -y >> /var/log/update_script.log
 echo '\n' >> /var/log/update_script.log
