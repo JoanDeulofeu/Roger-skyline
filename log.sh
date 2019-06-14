f26342cd74a3bb2bde5f07a97eb072a2f40e3069
root <root@roger>
2019-06-14 13:42:25 +0200

    fini log.sh

--- log.sh
+++ log.sh
@@ -2,5 +2,10 @@
 #
 # log.sh
 
-sudo apt-get update > /var/log/update_script.log
-sudo apt-get upgrade > /var/log/update_script.log
+DATE=$(date +"%m-%d-%y")
+HOUR=$(date +"%r")
+
+echo $DATE $HOUR '\n' >> /var/log/update_script.log
+sudo apt-get update >> /var/log/update_script.log
+sudo apt-get upgrade >> /var/log/update_script.log
+echo '\n' >> /var/log/update_script.log
